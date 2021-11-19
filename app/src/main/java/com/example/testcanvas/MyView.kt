aecc9b9069f766c513abb570ff40da22021a7696
Ivan Petr <ivan.kiel@gmail.com>
2021-11-19 12:59:43 +0800

    moving circle

--- app/src/main/java/com/example/testcanvas/MyView.kt
+++ app/src/main/java/com/example/testcanvas/MyView.kt
@@ -1,7 +1,30 @@
 package com.example.testcanvas
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.view.MotionEvent
 import android.view.View
 
 class MyView(context: Context?) : View(context) {
+    val p = Paint();
+    var centerX = 50f
+    var centerY = 50f
+    override fun onDraw(canvas: Canvas?) {
+        p.setColor(Color.RED)
+        canvas?.apply {
+            drawColor(Color.YELLOW)
+            drawCircle(centerX,centerY,30f, p)
+        }
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        event?.apply {
+            centerX = x; centerY = y
+        }
+        invalidate()
+
+        return true
+    }
 }

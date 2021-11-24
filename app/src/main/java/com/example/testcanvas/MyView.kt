2a92352e6a7ee68f5f5f67dc1ccb1fd07705f1e5
Ivan Petr <ivan.kiel@gmail.com>
2021-11-24 13:16:46 +0800

    TODO: task tiles generation

--- app/src/main/java/com/example/testcanvas/MyView.kt
+++ app/src/main/java/com/example/testcanvas/MyView.kt
@@ -4,24 +4,44 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 import java.util.*
 
+// TODO: реализовать класс-прямоугольник
 class MyView(context: Context?) : View(context) {
     val p = Paint(); val r = Random()
+    var layoutWidth = -1; var layoutHeight = -1
+
     // массив из логических значений  4*4
     val tiles = Array(4) { BooleanArray(4) { r.nextBoolean() } }
+    val padding = 10
+    lateinit var tilesRect: Array<Array<Rect>>
     var centerX = 50f
     val rect = Rect(0.1f, 0.1f, 0.1f, 0.1f)
     var centerY = 50f
     var w = 1000
     var h = 1000
 
+    override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
+        super.onLayout(changed, left, top, right, bottom)
+        layoutWidth = right - left; layoutHeight = bottom - top
+        // TODO: сгенерировать массив прямоугольников, зная размеры
+
+
+    }
+
     override fun onDraw(canvas: Canvas?) {
+        // сравнить размеры, полученные в onMeasure с тем,
+        // что возвращает canvas (width, height)
+
         p.setColor(Color.RED);
         // TODO: нарисовать плитки двух цветов, состояния которых заданы в массиве tiles
+
         canvas?.apply {
+            Log.d("mytag", "onDraw: w ${canvas.width}, h ${canvas.height}")
+            Log.d("mytag", "onLayout: w ${layoutWidth}, h ${layoutHeight}")
             w = width; h = height
             drawColor(Color.YELLOW)
             drawCircle(centerX,centerY,30f, p)
--- app/src/main/java/com/example/testcanvas/Rect.kt
+++ app/src/main/java/com/example/testcanvas/Rect.kt
@@ -1,4 +1,6 @@
 package com.example.testcanvas
 
 data class Rect(val x: Float, val y: Float, val width: Float, val height: Float) {
+fun isInto(pointX: Float, pointY: Float ): Boolean = false
+
 }
